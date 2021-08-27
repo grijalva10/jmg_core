229e294af31b70c6d2ead5e13802878846a12c58
Ubuntu <ubuntu@ip-172-31-25-109.us-west-2.compute.internal>
2021-08-27 20:54:34 +0000

    dang

--- jmg_core/jmg_core/doctype/prospect/prospect.js
+++ jmg_core/jmg_core/doctype/prospect/prospect.js
@@ -1,9 +1,18 @@
 // Copyright (c) 2021, JMG and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Prospect', {
-	// refresh: function(frm) {
-
-	// }
 	
+frappe.ui.form.on('Prospect', {
+    
+    onload(frm){
+       
+        
+    },
+    refresh(frm){
+         frm.add_custom_button('Test', frm.set_intro(frappe.format(frm.get_selected(), { fieldtype: 'Data' })));
+         frappe.show_progress('title', 10, 100, 'description');
+         
+         frm.get_value()
+
+    }
 });
