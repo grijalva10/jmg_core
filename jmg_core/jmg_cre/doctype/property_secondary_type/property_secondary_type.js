b2450785524c89b8cf86ec644801aad4d034d239
Ubuntu <ubuntu@ip-172-31-25-109.us-west-2.compute.internal>
2021-09-08 18:44:55 +0000

    changed directories

---
+++ jmg_core/jmg_cre/doctype/property_secondary_type/property_secondary_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, JMG and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Property Secondary Type', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ jmg_core/jmg_cre/doctype/property_secondary_type/test_property_secondary_type.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, JMG and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestPropertySecondaryType(unittest.TestCase):
+	pass
